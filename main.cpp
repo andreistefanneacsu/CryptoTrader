3e8429eac15b2b3bc060502fb5985116baa4cce3
andreistefanneacsu <andrei-stefan.neacsu@s.unibuc.ro>
2025-11-01 20:09:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,27 +11,21 @@ private:
     string simbol;
     double pret;
     double volatilitate;
+
 public:
-    Moneda() =default;
-    Moneda(const string &nume, const string &simbol, double pret, double volatilitate)
-        : nume(nume),
-          simbol(simbol),
-          pret(pret),
-          volatilitate(volatilitate) {
-    }
-    Moneda(const Moneda &moneda)
-        : nume(moneda.nume),
-          simbol(moneda.simbol),
-          pret(moneda.pret),
-          volatilitate(moneda.volatilitate) {
-    }
+    Moneda() = default;
+    Moneda(const string& nume, const string& simbol, double pret, double volatilitate)
+        : nume(nume), simbol(simbol), pret(pret), volatilitate(volatilitate) {}
+
+    Moneda(const Moneda& moneda) = default;  // Compiler-generated copy constructor
+
     ~Moneda() = default;
 
-    void set_nume(const string &nume) {
+    void set_nume(const string& nume) {
         this->nume = nume;
     }
 
-    void set_simbol(const string &simbol) {
+    void set_simbol(const string& simbol) {
         this->simbol = simbol;
     }
 
@@ -59,22 +53,16 @@ public:
         return volatilitate;
     }
 
-    void actualizarePret (double procent) {
-        this->pret = pret*(1+procent/100.0);
+    void actualizarePret(double procent) {
+        this->pret = pret * (1 + procent / 100.0);
     }
 
-    friend ostream& operator << (ostream& os, const Moneda& moneda) {
+    friend ostream& operator <<(ostream& os, const Moneda& moneda) {
         os << moneda.nume << " (" << moneda.simbol << ")" << ": " << moneda.pret << "EUR";
         return os;
     }
 
-    Moneda& operator=(const Moneda& moneda) {
-        this->nume = moneda.nume;
-        this->simbol = moneda.simbol;
-        this->pret = moneda.pret;
-        this->volatilitate = moneda.volatilitate;
-        return *this;
-    }
+    Moneda& operator=(const Moneda& moneda) = default;  // Compiler-generated assignment operator
 };
 
 class Tranzactie {
@@ -83,20 +71,17 @@ private:
     string tip;
     double cantitate;
     bool status;
+
 public:
     Tranzactie() = default;
-    Tranzactie(const Moneda moneda, const string tip, const double cantitate, const bool status)
-        : moneda(moneda),
-          tip(tip),
-          cantitate(cantitate),
-          status(status) {
-    }
+    Tranzactie(const Moneda& moneda, const string& tip, double cantitate, bool status)
+        : moneda(moneda), tip(tip), cantitate(cantitate), status(status) {}
 
     [[nodiscard]] Moneda get_moneda() const {
         return moneda;
     }
 
-    void set_moneda(const Moneda &moneda) {
+    void set_moneda(const Moneda& moneda) {
         this->moneda = moneda;
     }
 
@@ -104,7 +89,7 @@ public:
         return tip;
     }
 
-    void set_tip(const string &tip) {
+    void set_tip(const string& tip) {
         this->tip = tip;
     }
 
@@ -117,7 +102,7 @@ public:
     }
 
     double get_valoareTranzactie() const {
-        return cantitate*moneda.get_pret();
+        return cantitate * moneda.get_pret();
     }
 
     [[nodiscard]] bool get_status() const {
@@ -128,55 +113,55 @@ public:
         this->status = status;
     }
 
-    friend ostream& operator << (ostream& os, const Tranzactie& tranzactie) {
-        os << "[" << tranzactie.tip << "]: " << tranzactie.moneda.get_simbol() << " " << tranzactie.cantitate<< " "<< tranzactie.status;
+    friend ostream& operator <<(ostream& os, const Tranzactie& tranzactie) {
+        os << "[" << tranzactie.tip << "]: " << tranzactie.moneda.get_simbol() << " " << tranzactie.cantitate << " " << tranzactie.status;
         return os;
     }
 };
 
 class Portofel {
 private:
-    vector <pair<Moneda,double>> monede;
+    vector<pair<Moneda, double>> monede;
+
 public:
     Portofel() = default;
 
     Portofel(const vector<Tranzactie>& tranzactii) {
-        for (vector<Tranzactie>:: const_iterator tranzactie = tranzactii.begin(); tranzactie != tranzactii.end(); ++tranzactie) {
-            if (tranzactie->get_status()==true)
-            {
+        for (const auto& tranzactie : tranzactii) {  // Range-based loop for better clarity
+            if (tranzactie.get_status()) {
                 bool moneda_gasita = false;
-                for (vector <pair<Moneda,double>>:: iterator moneda = monede.begin(); moneda != monede.end(); ++moneda) {
-                    if (moneda->first.get_simbol()==tranzactie->get_moneda().get_simbol()) {
-                        if (tranzactie->get_tip()=="BUY") moneda->second+=tranzactie->get_cantitate();
-                        else if (tranzactie->get_tip()=="SELL") moneda->second-=tranzactie->get_cantitate();
+                for (auto& moneda : monede) {  // Use reference to avoid unnecessary copies
+                    if (moneda.first.get_simbol() == tranzactie.get_moneda().get_simbol()) {
+                        if (tranzactie.get_tip() == "BUY") moneda.second += tranzactie.get_cantitate();
+                        else if (tranzactie.get_tip() == "SELL") moneda.second -= tranzactie.get_cantitate();
                         moneda_gasita = true;
                         break;
                     }
                 }
-                if (!moneda_gasita&&tranzactie->get_tip()=="BUY") monede.push_back({tranzactie->get_moneda(),tranzactie->get_cantitate()});
+                if (!moneda_gasita && tranzactie.get_tip() == "BUY")
+                    monede.emplace_back(tranzactie.get_moneda(), tranzactie.get_cantitate());  // Use emplace_back
             }
         }
     }
 
     bool cumparaMoneda(const Moneda& m, double cantitate) {
-        for (vector <pair<Moneda,double>>:: iterator moneda = monede.begin(); moneda != monede.end(); ++moneda) {
-            if (moneda->first.get_simbol()==m.get_simbol()) {
-                moneda->second+=cantitate;
+        for (auto& moneda : monede) {  // Use reference to avoid unnecessary copies
+            if (moneda.first.get_simbol() == m.get_simbol()) {
+                moneda.second += cantitate;
                 return true;
             }
         }
-        monede.push_back({m,cantitate});
+        monede.emplace_back(m, cantitate);  // Use emplace_back
         return true;
     }
 
     bool vindeMoneda(const Moneda& m, double cantitate) {
-        for (vector <pair<Moneda,double>>:: iterator moneda = monede.begin(); moneda != monede.end(); ++moneda) {
-            if (moneda->first.get_simbol()==m.get_simbol()) {
-                if (moneda->second>=cantitate) {
-                    moneda->second-=cantitate;
+        for (auto& moneda : monede) {
+            if (moneda.first.get_simbol() == m.get_simbol()) {
+                if (moneda.second >= cantitate) {
+                    moneda.second -= cantitate;
                     return true;
-                }
-                else {
+                } else {
                     cout << "Tranzactia a esuat intrucat nu ai suficiente monezi!\n";
                     return false;
                 }
@@ -186,9 +171,9 @@ public:
         return false;
     }
 
-    friend ostream& operator << (ostream& os, const Portofel& portofel) {
-        for (vector <pair<Moneda,double>>:: const_iterator moneda = portofel.monede.begin(); moneda != portofel.monede.end(); ++moneda)
-            os<<moneda->first<<" "<<moneda->second<<"\n";
+    friend ostream& operator <<(ostream& os, const Portofel& portofel) {
+        for (const auto& moneda : portofel.monede)
+            os << moneda.first << " " << moneda.second << "\n";
         return os;
     }
 };
@@ -197,42 +182,41 @@ class Utilizator {
 private:
     string nume;
     Portofel portofel;
-    vector <Tranzactie> tranzactii;
+    vector<Tranzactie> tranzactii;
 
 public:
     Utilizator() = default;
-    Utilizator(const string &nume)
-        :nume(nume) {
+    Utilizator(const string& nume)
+        : nume(nume) {}
+
+    void cumpara(const Moneda& moneda, double cantitate) {
+        bool status = portofel.cumparaMoneda(moneda, cantitate);
+        tranzactii.push_back(Tranzactie(moneda, "BUY", cantitate, status));
     }
 
-    void cumpara (const Moneda& moneda, double cantitate) {
-        bool status=portofel.cumparaMoneda(moneda,cantitate);
-        tranzactii.push_back(Tranzactie(moneda,"BUY",cantitate,status));
-    }
-    void vinde (const Moneda& moneda, double cantitate) {
-        bool status=portofel.vindeMoneda(moneda,cantitate);
-        tranzactii.push_back(Tranzactie(moneda,"SELL",cantitate,status));
+    void vinde(const Moneda& moneda, double cantitate) {
+        bool status = portofel.vindeMoneda(moneda, cantitate);
+        tranzactii.push_back(Tranzactie(moneda, "SELL", cantitate, status));
     }
 
-    friend ostream& operator << (ostream& os, const Utilizator& utilizator) {
-        os<<utilizator.nume<<":\n";
-        os<<utilizator.portofel;
-        for (vector<Tranzactie>:: const_iterator tranzactie = utilizator.tranzactii.begin(); tranzactie != utilizator.tranzactii.end(); ++tranzactie)
-            os<<*tranzactie<<"\n";
+    friend ostream& operator <<(ostream& os, const Utilizator& utilizator) {
+        os << utilizator.nume << ":\n";
+        os << utilizator.portofel;
+        for (const auto& tranzactie : utilizator.tranzactii)  // Range-based loop for clarity
+            os << tranzactie << "\n";
         return os;
     }
-
 };
 
 int main() {
     Moneda btc("Bitcoin", "BTC", 68000, 5.2);
     Moneda new_btc;
     new_btc = btc;
-    btc.actualizarePret(-2.5);
+    btc.actualizarePret(-2.5);  // Decrease by 2.5%
     cout << btc << endl;
     cout << new_btc << endl;
 
-    Tranzactie t1(btc, "BUY", 5,true);
+    Tranzactie t1(btc, "BUY", 5, true);
     cout << t1.get_valoareTranzactie() << " EUR" << endl;
     cout << t1 << endl;
 
@@ -242,5 +226,6 @@ int main() {
 
     user.vinde(btc, 3);
     cout << user;
+
     return 0;
 }
